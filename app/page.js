53bb880822dcf9e414e5c7a817aa247b0c0ee854
Mayukh <mayukh1995@gmail.com>
2024-03-03 22:44:49 +0530

    updated home

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { fetchAllBlogs } from "@/lib/actions/blog.actions";
 import { useEffect, useState } from "react";
 import BlogCard from "@/components/BlogCard";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 const Home = () => {
   
@@ -24,7 +25,6 @@ const pathname = usePathname();
 
  return (
     <section id="home" className='relative'>
-    <div className='inset-0 bg-bg-image-1 bg-cover bg-repeat -z-30 absolute' />
     { session?.user?.id ? 
     <div className="flex flex-col ml-4 w-full items-center py-3" >
       <h1 className="text-4xl text-white font-bold" > Welcome {session?.user?.name}</h1>
@@ -44,8 +44,18 @@ const pathname = usePathname();
   </div>
     </div>
     :
-    <div className="flex flex-col ml-4 mt-4" >
-      <h1 className="text-4xl text-white font-bold" >Welcome to Blogger</h1>
+    <div className=" w-full justify-center flex ml-4 mt-4" >
+    <div className="flex flex-col gap-3" >
+    <h1 className="text-4xl text-white font-bold" >Welcome to Blogger</h1>
+    <h3 className="text-4xl text-white font-bold" >Post about the latest trending topics here</h3>
+     <Image
+      src="/hero_1.jpg"
+      height={650}
+      width={300}
+      priority
+      className="mt-5 object-contain"
+     />
+    </div>
     </div>
     }
     </section>
